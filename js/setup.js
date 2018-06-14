c4445ee6298e4cd2a8791f34bd31867dba673756
Oksana <ksish-ka@mail.ru>
2018-06-15 02:39:32 +0400

    внесены правки в наименования констант и функций

--- js/setup.js
+++ js/setup.js
@@ -3,20 +3,18 @@
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', ' Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARD_NUMBER = 4;
 
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
+var setup = document.querySelector('.setup').classList.remove('hidden');
 
-var setupSimilar = document.querySelector('.setup-similar');
-setupSimilar.classList.remove('hidden');
+var setupSimilar = document.querySelector('.setup-similar').classList.remove('hidden');
 
 
 // выбор случайного элемента
-var getRandomEl = function (array) {
+var getRandomValueFromArray = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
@@ -24,15 +22,16 @@ var getRandomEl = function (array) {
 // создание объекта мага
 var getWizard = function () {
   return {
-    name: getRandomEl(NAMES) + ' ' + getRandomEl(SURNAMES),
-    coatColor: getRandomEl(COAT_COLOR),
-    eyesColor: getRandomEl(EYES_COLOR)
+    name: getRandomValueFromArray(NAMES) + ' ' + getRandomValueFromArray(SURNAMES),
+    coatColor: getRandomValueFromArray(COAT_COLORS),
+    eyesColor: getRandomValueFromArray(EYES_COLORS)
   };
 };
 
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+console.log(similarWizardTemplate);
 
 // создание уникального мага
 
