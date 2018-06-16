ae74eb77d774e77b7e6fda790b6ed8cf38c1f70d
Keks <keks@htmlacademy.ru>
2018-06-16 12:36:48 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/setup.js
+++ 3/js/setup.js
@@ -3,37 +3,32 @@
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', ' Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARDS_AMOUNT = 4;
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NUMBER = 4;
 
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-var setupSimilar = document.querySelector('.setup-similar');
-setupSimilar.classList.remove('hidden');
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 
 
 // выбор случайного элемента
-var getRandomEl = function (array) {
-  var randomEl = array[Math.floor(Math.random() * array.length)];
-  return randomEl;
+var getRandomValueFromArray = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
 
 // создание объекта мага
 var getWizard = function () {
-  var wizard = {
-    name: getRandomEl(NAMES) + ' ' + getRandomEl(SURNAMES),
-    coatColor: getRandomEl(COAT_COLOR),
-    eyesColor: getRandomEl(EYES_COLOR)
+  return {
+    name: getRandomValueFromArray(NAMES) + ' ' + getRandomValueFromArray(SURNAMES),
+    coatColor: getRandomValueFromArray(COAT_COLORS),
+    eyesColor: getRandomValueFromArray(EYES_COLORS)
   };
-  return wizard;
 };
 
 
-var similarListElement = document.querySelector('setup-similar-list');
+var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 // создание уникального мага
@@ -49,10 +44,9 @@ var renderWizard = function (wizard) {
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < WIZARDS_AMOUNT; i++) {
+for (var i = 0; i < WIZARD_NUMBER; i++) {
   fragment.appendChild(renderWizard(getWizard()));
 }
 
 
 similarListElement.appendChild(fragment);
-
